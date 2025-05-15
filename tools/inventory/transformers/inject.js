78964735225ad9c6aa6a7edabfd543b5c2bab5ac
David Catalan <40354404+catalan-adobe@users.noreply.github.com>
2025-05-15 23:01:23 +0200

    Delete tools/inventory/transformers/inject.js

--- tools/inventory/transformers/inject.js
+++
@@ -1,6 +0,0 @@
-
-(() => {
-  document.querySelectorAll('.container-fluid.fixed-top.px-0.ct-header-basic, header.ct-header-basic-redesign, .container-fluid.ct-breadcrumb-a, .ct-header-basic-secondary').forEach((el) => {
-    el.style.position = 'static';
-  });
-})();
